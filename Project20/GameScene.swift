0a60f17a2b1538b0724a1c23440410a8c4e37fc7
Kirsty Darbyshire <kirsty@loquax.com>
2019-04-16 17:23:02 +0100

    challenge 1

--- Project20/GameScene.swift
+++ Project20/GameScene.swift
@@ -17,9 +17,10 @@ class GameScene: SKScene {
     var rightEdge = 1024 + 22
     var bottomEdge = -22
     
+    var scoreLabel: SKLabelNode!
     var score = 0 {
         didSet {
-            // score update code
+            scoreLabel.text = "Score: \(score)"
         }
     }
     
@@ -30,6 +31,12 @@ class GameScene: SKScene {
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+        
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.color = .cyan
+        scoreLabel.position = CGPoint(x: 30, y: 30)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
         
         gameTimer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
         
@@ -144,6 +151,45 @@ class GameScene: SKScene {
                 firework.removeFromParent()
             }
         }
+    }
+    
+    func explode(firework: SKNode) {
+        if let emitter = SKEmitterNode(fileNamed: "explode") {
+            emitter.position = firework.position
+            addChild(emitter)
+        }
+        firework.removeFromParent()
+    }
+    
+    func explodeFireworks() {
+        var numExploded = 0
+        
+        for (index, fireworkContainer) in fireworks.enumerated().reversed() {
+            guard let firework = fireworkContainer.children.first as? SKSpriteNode else { continue }
+            
+            if firework.name == "selected" {
+                explode(firework: fireworkContainer)
+                numExploded += 1
+                fireworks.remove(at: index)
+            }
+        }
+        
+        switch numExploded {
+        case 0:
+            break
+        case 1:
+            score += 200
+        case 2:
+            score += 500
+        case 3:
+            score += 1500
+        case 4:
+            score += 2500
+        default:
+            score += 4000
+            
+        }
+        
     }
     
 }
